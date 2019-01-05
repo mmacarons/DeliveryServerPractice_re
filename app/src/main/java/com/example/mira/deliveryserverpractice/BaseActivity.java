f8f6886f848a1412e61a77d02bef6606e8998887
mmacarons <manymany.macarons@gmail.com>
2019-01-05 21:52:45 +0900

    Initial commit

---
+++ app/src/main/java/com/example/mira/deliveryserverpractice/BaseActivity.java
@@ -0,0 +1,12 @@
+package com.example.mira.deliveryserverpractice;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+
+public abstract class BaseActivity extends AppCompatActivity {
+    public Context mContext = this;
+
+    public abstract void setupEvents();
+    public abstract void setValues();
+    public abstract void bindViews();
+}
